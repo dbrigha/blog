2579a1c91dd1682b65a1d0ca9a79c80898be986e
dbrigha <dbrigha@gmail.com>
2021-11-13 19:18:03 -0700

    mock possible blog post card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  blogImage = '../../../assets/images/blog_image.jpg';
+  blogPostImage = '../../../assets/images/you_are_here.jpg';
   constructor() { }
 
   ngOnInit(): void {
